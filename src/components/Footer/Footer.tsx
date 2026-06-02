9506ee17b5b324bfff7d739e8c5a855c42004b9b
mohamedamr122 <mohamedamrss123@gmail.com>
2026-06-02 04:35:08 +0300

    fix footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,6 @@
 import { motion } from "framer-motion";
-import {
-  fadeInUp,
-  staggerContainer,
-} from "../../animations/variants";
+import { fadeInUp, staggerContainer } from "../../animations/variants";
+
 const Footer = () => {
   return (
     <motion.footer variants={staggerContainer}>
@@ -15,13 +13,39 @@ const Footer = () => {
       >
         <p className="tracking-[-0.7px] font-black text-sm">ARCHITECT.DEV</p>
         <ul className="flex flex-wrap md:flex-nowrap gap-4 md:gap-10 text-text font-medium text-xs justify-center md:justify-start">
-          <li>Source Code</li>
-          <li>Privacy</li>
-          <li>LinkedIn</li>
-          <li>GitHub</li>
+          <li
+            onClick={() => {
+              window.open(
+                "https://github.com/MohamedAbdElsamieCoding/portfolio",
+                "_blank",
+              );
+            }}
+          >
+            Source Code
+          </li>
+          <li
+            onClick={() => {
+              window.open(
+                "https://www.linkedin.com/in/mohamed-abd-elsamie/",
+                "_blank",
+              );
+            }}
+          >
+            LinkedIn
+          </li>
+          <li
+            onClick={() => {
+              window.open(
+                "https://github.com/MohamedAbdElsamieCoding",
+                "_blank",
+              );
+            }}
+          >
+            GitHub
+          </li>
         </ul>
         <p className="text-text tracking-[1.2px] font-medium text-xs text-center md:text-right">
-          © 2024 ARCHITECT.DEV — ENGINEERED WITH PRECISION
+          © 2026 ARCHITECT.DEV — ENGINEERED WITH PRECISION
         </p>
       </motion.div>
     </motion.footer>
